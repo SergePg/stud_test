9c9e0555242da1f32e4979338b305088b93e149e
SergePg <sergekulik@gmail.com>
2016-06-24 09:02:12 +0300

    fixed Hello World UI initialization

--- modules/frontend/src/main/java/ua/org/pius/studtrain/ui/Testing.java
+++ modules/frontend/src/main/java/ua/org/pius/studtrain/ui/Testing.java
@@ -2,19 +2,25 @@ package ua.org.pius.studtrain.ui;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by SKulik on 23.06.2016.
  */
-@Component
+@Component("testingUI")
 @Scope("prototype")
 @Theme("testingTheme")
 public class Testing extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-
+        VerticalLayout layout = new VerticalLayout();
+        Label label = new Label("Hello world!");
+        layout.setSizeFull();
+        layout.addComponent(label);
+        setContent(layout);
     }
 }
